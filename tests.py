7558f50270efae3d51ee8110b0dadca9bb1f2c41
marto-test <50634134+marto-test@users.noreply.github.com>
2019-05-14 15:05:12 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+import test_directory as awesome
+
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(awesome.smile(), ":)")
+
+
+if __name__ == '__main__':
+    unittest.main()
