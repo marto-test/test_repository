78a7f033ef0b34555625ec0d5a4ec383adcb0509
marto-test <marto.test@yahoo.com>
2019-05-14 17:24:07 -0400

    updates

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(awesome.function_1(), True)
 
     def test_function_2(self):
-        self.assertEqual(awesome.function_2(), False)
+        self.assertEqual(awesome.function_2(), True)
 
 
 if __name__ == '__main__':
