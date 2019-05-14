9c89374e602ea2bdf9ff96c282170ab29a1c69d3
marto-test <marto.test@yahoo.com>
2019-05-14 15:48:57 -0400

    u

--- tests.py
+++ tests.py
@@ -6,7 +6,8 @@ import test_repository.test_module as awesome
 class TestMethods(unittest.TestCase):
     def test_function_1(self):
         self.assertEqual(awesome.function_1(), True)
-
+    def test_function_2(self):
+	self.assertEquat(awesome.function_2(), True)
 
 if __name__ == '__main__':
     unittest.main()
